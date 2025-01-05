0481f96a379861d350a9e010418738e07fb21072
Emre Avara <emre.avara94@gmail.com>
2025-01-05 13:51:53 +0100

    v1

---
+++ src/Main.java
@@ -0,0 +1,5 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("degisiklik 2"); //ws, git tarafindan daha takip edilmiyor.
+    }
+}
---
+++ src/Runner.java
@@ -0,0 +1,6 @@
+public class Runner {
+    public static void main(String[] args) {
+        //Versiyon 1
+        System.out.println("degisiklik 1"); //Working space
+    }
+}
