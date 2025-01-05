885a66454f945984196fd0fd7e5fe1b7611b7149
Emre Avara <emre.avara94@gmail.com>
2025-01-05 13:59:03 +0100

    v2

--- src/Runner.java
+++ src/Runner.java
@@ -1,6 +1,9 @@
 public class Runner {
     public static void main(String[] args) {
         //Versiyon 1
-        System.out.println("degisiklik 1"); //Working space
+        System.out.println("degisiklik 1"); //Working space, git tarafindan takibe alinmama demek.
+
+        //Versiyon 2
+        System.out.println("degisiklik yaptim");
     }
 }
